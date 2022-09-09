ec8e3da9db4b19b68e12c92860569e9b4579c4d6
Julius Kamara <juliuskamara79@gmail.com>
2022-09-08 23:32:45 -0700

    Added: a new C file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/* This program prints exactly a line of text */
+#include <stdio.h>
+/**
+ * main- without the main function, our C program is not functionable
+ * @void:represents that the function takes no argument
+ *Return:returns zero, which is used passed to the exit status value as zero
+ */
+int main(void)
+{
+	printf("%s", " with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
