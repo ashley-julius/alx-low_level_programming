6de1ad6d7f853294006730a601e8a02ad0f70e3d
Julius Kamara <juliuskamara79@gmail.com>
2022-09-08 19:35:58 -0700

    Added: a new file

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -7,15 +7,6 @@
  */
 int main(void)
 {
-  return (0);
-}#include <stdio.h>
-
- /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
-int main(void)
-{
+  puts("Julius is doing it everyday");
   return (0);
 }
