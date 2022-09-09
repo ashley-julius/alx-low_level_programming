e2b98e5210de3fe0f13d6359eb4cbd37497a9381
Julius Kamara <juliuskamara79@gmail.com>
2022-09-08 22:27:17 -0700

    Added a new file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+ /* This program just tells us what programming is like */
+#include <stdio.h>
+/**
+ * main- without the main function, our C program is not functionable
+ * @void:represents that the function takes no argument
+ *Return:returns zero, which is used passed to the exit status value as zero
+ */
+int main(void)
+{
+	puts("programming is like a multilingual puzzle\n");
+	(return 0;)
+}
