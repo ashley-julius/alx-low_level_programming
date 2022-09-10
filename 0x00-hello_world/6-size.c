dcdaf44b1aa743584777e017facd18df7e3a1778
Julius Kamara <juliuskamara79@gmail.com>
2022-09-10 09:47:54 -0700

    Added a new file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	/*Print the size of a char in byte */
-	printf("Size of a char: %i %s", 1 "byte(s)\n");
-	/*Print the size of an int in bytes */
-	printf("Size of an int: %i %s", 4 "byte(s)\n");
-	/*Print the size of a long int in bytes */
-	printf("Size of a long int: %i %s", 4 "byte(s)\n");
-	/*Print the size of a long long int in bytes */
-	printf("Size of a long long int: %i %s", 8 "byte(s)\n");
-	/*Print the size of a float in byte */
-	printf("Size of a float: %i %s", 4 "byte(s)");
+	/*Display the size of a char in byte */
+	puts("Size of a char: 1 byte(s)");
+	/*Display the size of an int in bytes */
+	puts("Size of an int: 4 byte(s)");
+	/*Display the size of a long int in bytes */
+	puts("Size of a long int: 8 byte(s)");
+	/*Display the size of a long long int in bytes */
+	puts("Size of a long long int: 8 byte(s)");
+	/*Display the size of a float in byte */
+	puts("Size of a float: 4 byte(s)");
 
 	return (0);
 }
