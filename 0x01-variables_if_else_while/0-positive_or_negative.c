446bd5a0ba4c76209808a7ac0ef3824d22a8fbbe
Julius Kamara <juliuskamara79@gmail.com>
2022-09-09 20:49:45 -0700

    Added a new C file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- This is where code starts
+ * @void: means no argument
+ * Return: zero
+*/
+int main(void)
+{
+	/* define n of type int */
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* checks if n is bigger than zero */
+	if (n > 0)
+		/*
+		 * prints the message if condition is true
+		*/
+		printf("%d is positive", n);
+	/* checks if n is zero */
+	else if (n == 0)
+		/*
+		 * prints the message if condition is true
+		 */
+		printf("%d is zero", n);
+	/* otherwise n is a negative number */
+	else
+		/*
+		 * prints the message if condition is true
+		 */
+		printf("%d is negative", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project is all about C variables, if else and while loop
