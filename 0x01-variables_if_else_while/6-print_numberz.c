42e6577d75de51c2b5127a8153777a1bbf2f3289
Julius Kamara <juliuskamara79@gmail.com>
2022-09-11 16:19:29 -0700

    Added a new file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/* This program prints the base ten digits
+ * starting from using only putchat
+ */
+#include <stdio.h>
+/**
+ * main- This is where our code starts
+ * @void- this means main has no arguments
+ * Return: zero
+ */
+int main(void)
+{
+	/*
+	 *  define a new char named ch 
+	 */
+	char ch;
+
+	/*
+	 * loop
+	 */
+	for (ch = '1'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
