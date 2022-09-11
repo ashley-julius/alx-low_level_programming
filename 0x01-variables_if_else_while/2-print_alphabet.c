ec4dda99935cd0171c919020fb685d008806c3ba
Julius Kamara <juliuskamara79@gmail.com>
2022-09-10 17:40:58 -0700

    Added: a new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+/* This program converts the alphabets to lowercase */
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main-This where our code starts
+ * @void-main takes no argument
+ * Return: zero
+ */
+int main(void)
+{
+	/*
+	 * container for each char when we loop
+	 */
+	char chr;
+
+	/*
+	 * loop
+	 */
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		chr = tolower(chr);
+		putchar(chr);
+	}
+	return (0);
+}
