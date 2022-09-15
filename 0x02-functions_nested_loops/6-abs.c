36af2033f965973a9d1f55a33cda047894787983
Julius Kamara <juliuskamara79@gmail.com>
2022-09-14 18:34:49 -0700

    ALL

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/*
+* This program prints the
+* absolute value of an int
+*/
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * * _abs- print a single character
+ * * to the screen or stdout
+ * * @num: takes a int parameter
+ * * Return: the character
+ */
+int _abs(int num)
+{
+	return (_abs(num));
+}
