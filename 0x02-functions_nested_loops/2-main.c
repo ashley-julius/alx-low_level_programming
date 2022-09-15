8e6bd576eb378fb11d8586839c43e5fc5afee38e
Julius Kamara <juliuskamara79@gmail.com>
2022-09-14 17:31:30 -0700

    ALL

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -9,10 +9,18 @@
  */
 int main(void)
 {
+	int r;
+
 	/*
 	 * invoking the print_alphabet function
 	 */
-	print_alphabet_x10();
 
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+/* This program checks for lowercase character */
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+
+
+/**
+ * _islower- checks if it is lowercase
+ * and return 1 or 0
+ * @c: accepts an int
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
+
