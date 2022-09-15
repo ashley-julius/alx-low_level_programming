e588a36bd5229a8dd0a270f708d47008f96ccce4
Julius Kamara <juliuskamara79@gmail.com>
2022-09-15 00:14:27 -0700

    ALL

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,16 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i < 98)
+			if (i != 98)
 				printf("%d, ", i);
-
 			else
 				printf("%d\n", i);
 		}
@@ -29,8 +32,12 @@ void print_to_98(int n)
 
 	else if (n > 98)
 	{
-		for (i = n; i > 98; i--)
-			printf("%d, ", i);
+		for (i = n; i >= 98; i--)
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d\n", i);
 	}
 
+	_putchar('\n');
 }
