62e2b5795548c6e4c21a39b5da2c48c4c177c913
Julius Kamara <juliuskamara79@gmail.com>
2022-09-16 18:54:57 -0700

    ALL

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,17 +11,22 @@
  */
 void print_diagonal(int n)
 {
-	if (n == 0 && n < 0)
+	int i, k;
+
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		while (n > 0)
+		for (i = 0; i < n; i--)
 		{
+			for (k = 0; k < i; k++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
-			n--;
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
