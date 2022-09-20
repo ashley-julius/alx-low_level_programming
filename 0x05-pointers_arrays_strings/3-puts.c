2da2a43fc4f2041b105b5d31b657c0f62af6c52b
Julius Kamara <juliuskamara79@gmail.com>
2022-09-20 13:31:19 -0700

    Add: a new file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+/*
+ * This program prints a string
+ * and a newline
+ */
+#include "main.h"
+/**
+ * _puts - print a bunch of string
+ * @s: pointer to char
+ */
+void _puts(char *str)
+{
+	/*
+	 * Declare counter loop
+	 */
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
