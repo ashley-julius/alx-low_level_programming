24eccbe2a174ebe1f1bfe1adc55e52162ea13b65
Julius Kamara <juliuskamara79@gmail.com>
2022-09-19 18:35:47 -0700

    Added a new file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * This program update the value its parameters
+ * points to
+ */
+/**
+ * reset_to_98 - updates the data in memory with the
+ * address passed as parameter
+ * @n:pointer to an integer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
