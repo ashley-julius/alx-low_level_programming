1cafa1c3507be99c7318ae76b7fc240b4c812b58
Julius Kamara <juliuskamara79@gmail.com>
2022-09-25 20:49:10 -0700

    Add a file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,7 +13,7 @@ void puts2(char *str)
 	while (str[len] != '\0')
 		len++;
 
-	for (i = 0; i < len -1; i += 2)
-		_putchar(str[i]);
+	for (i = 0; i < len - 1; i += 2)
+		_putchar(*(str + i));
 	_putchar('\n');
 }
