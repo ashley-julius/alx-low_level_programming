39492240a2db05a24911968aa3314226eaff109c
Julius Kamara <juliuskamara79@gmail.com>
2022-09-27 16:55:58 -0700

    Add a new file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+/* This program returns a character in a string */
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - this function finds a string and
+ * returns it
+ * @s: pointer to a char
+ * @c: ordinary char
+ * Return: (s + i) or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return ((s + i));
+		i++;
+	}
+	return ((s + i));
+}
