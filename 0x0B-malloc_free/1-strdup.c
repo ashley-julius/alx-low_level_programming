b235ce568838c0a7ec3e6e1a8302a4e3f5a0862f
Julius Kamara <juliuskamara79@gmail.com>
2022-10-05 17:43:25 -0700

    Add a new file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,19 +9,24 @@
 char *_strdup(char *str)
 {
 	char *s;
-	int i, k;
+	int i, len, k;
 
 	if (str == NULL)
 		return (str);
 	i = 0;
+	len = 0;
 	while (str[i] != '\0')
+	{
 		i++;
+		len++;
+	}
 
-	s = malloc(sizeof(str));
+	s = malloc(len * sizeof(char));
 	if (s == NULL)
 		return (s);
-	for (k = 0; k <= i - 1; k++)
+	for (k = 0; k <= len; k++)
 		s[k] = str[k];
+	s[k] = '\0';
 	return (s);
 }
 
