2dd1980813cee07a9a50aa6c49a1e077c6c8e218
Julius Kamara <juliuskamara79@gmail.com>
2022-10-10 20:03:08 -0700

    Add a new file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,52 +5,35 @@
  * times_table - this is about print times table
  * @void: means no argument
  */
-
 void times_table(void)
 {
-	int i, j, k, add, first, last, count;
+	int i, k, first, last, count;
 
-	for (j = 0; j < 10; j++)
-	{
-		_putchar('0');
-		if (j < 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
-	}
-	_putchar('\n');
-	add = 1;
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (k = 0; k <= 9; k++)
 		{
-			count = add * k;
-			if (count <= 9)
-			{
-				_putchar(count + '0');
-				if (k < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-			}
+			count = i * k;
 			if (count > 9)
 			{
 				first = count / 10;
 				last = count % 10;
 				_putchar(first + '0');
 				_putchar(last + '0');
-				if (k < 9)
-				{
-					_putchar(',');
+			}
+			if (count <= 9)
+			{
+				_putchar(count + '0');
+			}
+			if (k < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (((k + 1) * i) < 10)
 					_putchar(' ');
-				}
+
 			}
 		}
 		_putchar('\n');
-		add++;
 	}
 }
