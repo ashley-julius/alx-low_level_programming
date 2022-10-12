4f4abd63bbf1d1e949963a893985dcac3da898cc
Julius Kamara <juliuskamara79@gmail.com>
2022-10-12 00:49:25 -0700

    First project on structures in C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H 
+/**
+ * struct dog - This structure provides a dog description
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: This structure identifies a dog
+ */
 struct dog
 {
 	char *name;
