820f024f1bb9a4ef20539fbc6805111f509d0001
Julius Kamara <juliuskamara79@gmail.com>
2022-10-12 02:49:55 -0700

    Added a new file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,13 @@
  *
  * Description: This structure identifies a dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+typedef struct dog dog_t;
 
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
