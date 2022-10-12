046846bd3ef7c046d20d1e696b143952cb868040
Julius Kamara <juliuskamara79@gmail.com>
2022-10-12 15:05:54 -0700

    Add a new file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - This function prints a name in many cases
+ * @name: string literal
+ * @f: pointer to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
