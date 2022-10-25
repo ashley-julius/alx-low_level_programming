3ac4c1e49ce06da3883f8e38e5bade5ed0d43aff
Julius Kamara <juliuskamara79@gmail.com>
2022-10-24 21:34:16 -0700

    This file shows us how to execute a function before the main program

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+/* This is a function that is executed before our main program */
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_before_main - This is a function that is printed before main
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
