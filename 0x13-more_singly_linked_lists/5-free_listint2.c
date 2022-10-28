e16d239aa68bd714b1dd51218e7b1be87bee6ed4
Julius Kamara <juliuskamara79@gmail.com>
2022-10-27 17:23:16 -0700

    This file frees all nodes from the heap and sets the head pointer to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - This function releases the node memory from the heap
+ *@head: poiner to a pointer to a struct
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p = *head;
+
+	while (*head != NULL)
+	{
+		p = p->next;
+		free(*head);
+		*head = p;
+	}
+}
