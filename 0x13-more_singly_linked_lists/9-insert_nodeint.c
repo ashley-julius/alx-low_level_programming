766476dee60b3167e12ab13de9cd06204f720f55
Julius Kamara <juliuskamara79@gmail.com>
2022-10-31 05:21:48 -0700

    Added a new file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,53 +1,43 @@
-/* This function adds a new node at a given location */
 #include "lists.h"
-#include <stdlib.h>
+
 /**
- * insert_nodeint_at_index - This function adds a new node at the given index
- * @head: head pointer to the first node
- * @idx: unsigned int
- * @n: ordinary int
- * Return: address of the newly selected node
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr = NULL;
-	listint_t *tmp = NULL;
-	listint_t *pto = NULL;
-	unsigned int i = 0;
+	listint_t *new, *copy = *head;
+	unsigned int node = 0;
 
-	if (*head == NULL)
-	{
-		return (*head);
-	}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
 
 	if (idx == 0)
 	{
-		pto = (listint_t *)malloc(sizeof(listint_t));
-		if (pto == NULL)
-			return (pto);
-		pto->n = n;
-		tmp = *head;
-		pto->next = tmp;
-		*head = pto;
-		return (pto);
+		new->next = copy;
+		*head = new;
+		return (new);
 	}
-	else
+
+	for (; node < (idx - 1); node++)
 	{
-		ptr = *head;
-		pto = (listint_t *)malloc(sizeof(listint_t));
-		if (pto == NULL)
-			return (pto);
-		pto->n = n;
-		for (; i < idx && ptr != NULL; i++)
-			ptr = ptr->next;
-		if (ptr->next == NULL)
+		if (copy == NULL || copy->next == NULL)
 			return (NULL);
-		tmp = ptr->next;
-		ptr->next = pto;
-		pto->next = tmp;
+
+		copy = copy->next;
 	}
-	return (pto);
+	new->next = copy->next;
+	copy->next = new;
+	return (new);
 }
-
-
-
