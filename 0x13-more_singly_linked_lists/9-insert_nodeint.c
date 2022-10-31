48fd6be7301d19e14172e2694985d7a58e4a9bca
Julius Kamara <juliuskamara79@gmail.com>
2022-10-31 05:25:02 -0700

    Added a new file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,21 +20,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
-
 	if (idx == 0)
 	{
 		new->next = copy;
 		*head = new;
 		return (new);
 	}
-
 	for (; node < (idx - 1); node++)
 	{
 		if (copy == NULL || copy->next == NULL)
 			return (NULL);
-
 		copy = copy->next;
 	}
 	new->next = copy->next;
