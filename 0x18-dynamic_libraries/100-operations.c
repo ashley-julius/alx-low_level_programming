c31a8a21cfe2d24d54efc1a72ea6d5fa0bc2a4ba
Julius Kamara <juliuskamara79@gmail.com>
2022-12-17 06:46:23 -0800

    This program shows one how to work with both python and c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/*_ _ _ _ THIS FUNCTION WORKS AS A CALCULATOR _ _ _ _ */
+/*_ _ _ _  GIVE ME VALUES TO PERFORM OPERATIONS_ _ _ _ */
+#include <stdio.h>
+
+/* _ _ _ SUM CALCULATOR _ _ */
+/**
+ * add - return the sum of two numbers
+ * @a: ordinary int
+ * @b: ordinary int
+ * Return: the product
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/*_ _ _ _ DIFFERENCE CALCULATOR */
+/**
+ * sub - return the difference between two numbers
+ * @a: ordinary int
+ * @b: ordinary int
+ * Return: the product
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/* _ _ _ PRODUCT CALCULATOR */
+/**
+ * mul - return the product of two numbers
+ * @a: ordinary int
+ * @b: ordinary int
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/* _ _ _ DIVISION CALCULATOR */
+/**
+ * div - retuns the quotient of the numbers
+ * @a: ordinary int
+ * @b: ordinary int
+ * Return: the product
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/*_ _ _ MODULE CALCULATOR___*/
+/**
+ * mod - returns the remainder after a division
+ * @a: ordinary int
+ * @b: ordinary int
+ * Return: the product
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
