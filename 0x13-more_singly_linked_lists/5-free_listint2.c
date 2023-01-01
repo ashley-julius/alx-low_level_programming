69065973c21ffd5895b8528809421cb66c7f6197
Julius Kamara <juliuskamara79@gmail.com>
2022-12-31 17:19:56 -0800

    This program shows how to delete the entire singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,10 +14,13 @@ void free_listint2(listint_t **head)
 		return;
 	}
 
-	while (p != NULL)
+	else
 	{
-		p = p->next;
-		free(*head);
-		*head = p;
+		while (p != NULL)
+		{
+			p = p->next;
+			free(*head);
+			*head = p;
+		}
 	}
 }
