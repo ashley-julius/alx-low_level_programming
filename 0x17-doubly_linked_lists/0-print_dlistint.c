f87675b2cc8a0a50eef9d8aa486e895080ea1702
Julius Kamara <juliuskamara79@gmail.com>
2023-01-20 09:08:18 -0800

    This project introduces us to doubly linked lists, how movement is done in a non-linear way

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,15 +3,19 @@
 /**
  * print_dlistint - This function prints all doubly linked list data
  * @h: pointer to the first node in the list
- * Return: 0
+ * Return: count
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t count;
+
+	count = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
-	return (0);
+	return (count);
 }
