e28ae4407b4b3cf8359ced5bc5da1e123b42d534
Julius Kamara <juliuskamara79@gmail.com>
2023-01-21 07:01:15 -0800

    This project adds a new node at a certain position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,42 +1,46 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - This function adds a new node
  * at a given position
- *
  * @h: pointer to the first node in the list
  * @idx: the position to add the new node
  * @n: data part of the node
  * Return: NULL or the new node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	/* declare the iterator variable and two pointers */
 	unsigned int i;
 	dlistint_t *ptr, *temp;
-	/*check if list is empty */
+
 	if (*h == NULL)
-		return (NULL);
-	ptr = *h;
-	/* loop through just before the position */
-	i = 0;
-	while (i < idx)
 	{
-		if (ptr->next == NULL)
-			return (NULL);
-		ptr = ptr->next;
-		i++;
-	}
-	/* create the new node */
-	temp = malloc(sizeof(dlistint_t));
-	/* check if malloc failed */
-	if (temp == NULL)
+		temp = malloc(sizeof(dlistint_t));
+		if (temp == NULL)
+			return (temp);
+		temp->n = n;
+		temp->next = NULL;
+		temp->prev = NULL;
+		*h = temp;
 		return (temp);
-	/* the data and link parts */
-	temp->n = n;
-	temp->next = ptr->next;
-	temp->prev = ptr;
-	ptr->next = temp;
-	return (temp);
+	}
+	else
+	{
+		ptr = *h;
+		i = 1;
+		while (i < idx)
+		{
+			if (ptr->next == NULL)
+				return (NULL);
+			ptr = ptr->next;
+			i++;
+		}
+		temp = malloc(sizeof(dlistint_t));
+		if (temp == NULL)
+			return (temp);
+		temp->n = n;
+		temp->next = ptr->next;
+		temp->prev = ptr;
+		ptr->next = temp;
+		return (temp);
+	}
 }
