27987da64227faba7e26ed860a9b3874c9f0538c
julius-ashley <juliuskamara79@gmail.com>
2023-04-29 21:17:33 -0700

    Fix some minor issue

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,6 +18,8 @@ int main(void)
 
 		if ((i % 5 == 0) && (i % 3 == 0))
 			printf("FizzBuzz ");
+		else if (i == 1)
+			printf("%d ", i);
 		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else if ((i % 5) == 0)
